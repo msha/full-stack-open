e1a033622d52463339a3a90f87fd7c875c75edfe
DESKTOP-BLYAT\Lauri <1960498+msha@users.noreply.github.com>
2024-03-13 08:40:58 +0200

    add part1 step2

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -4,10 +4,19 @@ const Header = (props) => {
 }
 const Content = (props) => {
   return (
-    <p>
-        {props.part} {props.exercises}
-      </p>
+    <div>
+      <Part part={props.parts[0]} exercises={props.exercises[0]}></Part>
+      <Part part={props.parts[1]} exercises={props.exercises[1]}></Part>
+      <Part part={props.parts[2]} exercises={props.exercises[2]}></Part>
+      </div>
 )}
+const Part = (props) => {
+  return (
+    <p>
+    {props.part} {props.exercises}
+    </p>
+  )
+}
 const Total = (props) => {return <p>Number of exercises {props.sum}</p>}
 const App = () => {
   const course = 'Half Stack application development'
@@ -21,9 +30,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}></Header>
-      <Content part={part1} exercises={exercises1}></Content>
-      <Content part={part2} exercises={exercises2}></Content>
-      <Content part={part3} exercises={exercises3}></Content>
+      <Content exercises={[exercises1,exercises2,exercises3]} parts={[part1,part2,part3]}></Content>
       <Total sum={exercises1+exercises2+exercises3}></Total>
     </div>
   )
