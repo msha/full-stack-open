cf4aaf80817b210955ad4893e14d5ff9e47c5545
DESKTOP-BLYAT\Lauri <1960498+msha@users.noreply.github.com>
2024-03-19 13:01:02 +0200

    add part2 step16

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -6,7 +6,17 @@ const Filter = ({filter, fun}) => {
   return (<div>filter shown with <input value={filter} onChange={fun}/></div>)
 }
 
+const Notification = ({ message, type }) => {
+  if (message === null) {
+    return null
+  }
 
+  return (
+    <div className={type}>
+      {message}
+    </div>
+  )
+}
 
 const Person = ({name, number, id, doDelete}) => {
   return (<div>{name} {number}<button onClick={() => {doDelete(id)}}>delete</button></div>)
@@ -48,8 +58,11 @@ const Form = ({addNewName,newName,newNumber,handleNameChange,handleNumberChange}
   )
 }
 
+
+
 const App = () => {
 
+  const [errorMessage, setErrorMessage] = useState({msg:'some error happened...',type:'error'})
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const handleNameChange = (event) => {
@@ -60,7 +73,14 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+
+  const setNotifyMessage = (msg,type) => {
+    setErrorMessage({msg: msg, type: type})
+    setTimeout(() => {setErrorMessage({msg: null, type: null})},5000)
+  }
+
   const doDelete = (id) => {
+    setNotifyMessage(`Deleted ${persons.find(p => p.id === id).name}`, 'delete');
     personServer.purge(id);
     setPersons(persons.filter(person => person.id !== id));
   }
@@ -70,15 +90,16 @@ const App = () => {
     if(!persons.map((o) => o.name).includes(newName)) {
       const newPerson = {name: newName, number: newNumber};
       personServer.create(newPerson).then((res) => setPersons(persons.concat(res)))
+      setNotifyMessage(`Added ${newName}`, 'notify');
     } else {
       const promptReplace = confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)
       if(promptReplace) {
-
         const newPerson = {name: newName, number: newNumber};
         const id = persons.filter((o) => o.name === newName)[0].id;
         personServer
           .update(id,newPerson)
           .then((res) => setPersons(persons.map(p => p.id !== id ? p : res)))
+          setNotifyMessage(`Updated ${newName}`, 'notify');
       }
     }
     setNewNumber('')
@@ -93,6 +114,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage.msg} type={errorMessage.type}></Notification>
       <Filter filter={filter} fun={handleFilter}></Filter>
       <h2>add a new</h2>
       <Form addNewName={addNewName} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}></Form>
